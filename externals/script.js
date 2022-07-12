2bf5bc052a6768bf4939d697c5cc40eebe9e3bba
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-07-12 12:18:31 +0600

    Add files via upload

--- externals/script.js
+++ externals/script.js
@@ -1,51 +1,54 @@
 // --> Setting up the Alarm audio <--
-const alarm = new Audio('alarmbell.mp3');
-
-// --> Fixing up the current time <--
+let alarm = new Audio('alarmbell.mp3');
 
 setInterval(() => {
 
-let currentDate = new Date();
-let currentHour = parseInt(currentDate.getHours());
-let currentMinutes = parseInt(currentDate.getMinutes());
-let dateOutput = currentHour + currentMinutes;
+    // --> Fixing up the current time <--
+    let currentDate = new Date();
+    let currentHour = parseInt(currentDate.getHours());
+    let currentMinutes = parseInt(currentDate.getMinutes());
+    let dateOutput = currentHour + currentMinutes;
 
-// --> Collecting data from user and storing up to the localStorage <--
-let setButton = document.getElementById('set');
-setButton.addEventListener('click', (e) => {
-    e.preventDefault();
+    // --> Displaying current time to the user
+    let currentTime = document.getElementById('currentTime');
+    currentTime.innerHTML = `Current Time  &#8594 <span class="time">${currentHour}:${currentMinutes}:${currentDate.getSeconds()}</span> (24Hrs format)`
 
-    let hours = parseInt(document.getElementById('hours').value);
-    let minutes = parseInt(document.getElementById('minutes').value);
+    // --> Collecting data from user and storing up to the localStorage <--
+    let setButton = document.getElementById('set');
+    setButton.addEventListener('click', (e) => {
+        e.preventDefault();
 
-    localStorage.setItem('hour', hours);
-    localStorage.setItem('minute', minutes);
+        let hours = parseInt(document.getElementById('hours').value);
+        let minutes = parseInt(document.getElementById('minutes').value);
 
-    let dateInput = hours + minutes;
-    localStorage.setItem('alarm', dateInput);
+        localStorage.setItem('hour', hours);
+        localStorage.setItem('minute', minutes);
 
-    swal("Owh Yoo!", "Your alarm has been saved!", "success");
-})
+        let dateInput = hours + minutes;
+        localStorage.setItem('alarm', dateInput);
 
-// --> Writing a statement to play the alarm <--
-let dateInput = parseInt(localStorage.getItem('alarm'));
-let div = document.getElementById('gif');
-if (dateInput == dateOutput) {
-    div.style.display = 'block';
-    alarm.play();
-}
+        swal("Owh Yoo!", "Your alarm has been saved!", "success");
+    })
 
-// --> Live Alarm status <--
+    // --> Writing a statement to play the alarm <--
+    let dateInput = parseInt(localStorage.getItem('alarm'));
+    let div = document.getElementById('gif');
+    if (dateInput == dateOutput) {
+        div.style.display = 'block';
+        alarm.play();
+    }
 
-let showHours = localStorage.getItem('hour');
-let showMinutes = localStorage.getItem('minute');
+    // --> Live Alarm status <--
 
-let info = document.getElementById('info');
-if (showHours==null) {
-    info.innerHTML = `No alarms yet! Try to save an alarm right now.`;
-} else {
-    info.innerHTML = ` You've previously set an alarm at &#8594 ${showHours}:${showMinutes} `;
-}
+    let showHours = localStorage.getItem('hour');
+    let showMinutes = localStorage.getItem('minute');
 
-console.log(dateInput, dateOutput);
+    let info = document.getElementById('info');
+    if (showHours == null) {
+        info.innerHTML = `No alarms yet! Try to save an <span class="time">alarm</span> right now.`;
+    } else {
+        info.innerHTML = ` You've previously set an alarm at &#8594 <span class="time">${showHours}:${showMinutes}</span> `;
+    }
+
+    console.log(dateInput, dateOutput);
 }, 1000);
